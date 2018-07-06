2907e37db2e98d082c3db3d51f1e4d32be8b1b2d
Daniela Passos <moitidani@gmail.com>
2018-07-06 12:20:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,22 +2,35 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element){
   //should return a new array and not modify the original
+<<<<<<< HEAD
   array = [element, ... array];
+=======
+  array = ['element', ... array];
+>>>>>>> 8c9450a3f7b6bd352ebc823e5b0f391bcea34f52
   return array
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
   //should alter the original array that's passed in; 
+<<<<<<< HEAD
   array.unshift(element)
+=======
+  array.unshift('element')
+>>>>>>> 8c9450a3f7b6bd352ebc823e5b0f391bcea34f52
   return array
 }
 
 function addElementToEndOfArray (array, element) {
+<<<<<<< HEAD
   array = [... array, element];
+=======
+  array = [... array, 'element'];
+>>>>>>> 8c9450a3f7b6bd352ebc823e5b0f391bcea34f52
   return array
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
+<<<<<<< HEAD
   array.push(element);
   return array
 }
@@ -42,4 +55,8 @@ function destructivelyRemoveElementFromEndOfArray  (array){
 
 function removeElementFromEndOfArray (array){
   return array.slice(0, array.length-1)
+=======
+  array.shift('element');
+  return array
+>>>>>>> 8c9450a3f7b6bd352ebc823e5b0f391bcea34f52
 }
